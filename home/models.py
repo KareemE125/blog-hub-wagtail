7c271407d6a3fdc7bc7fb21660c2342a76513b04
KareemE125 <kareemezzat.125@gmail.com>
2023-12-30 12:53:23 +0200

    add some fields + add bootstrap

--- home/models.py
+++ home/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+    max_count = 1
+    
+    description = RichTextField(max_length=255, blank=True)
+    image = models.ForeignKey("wagtailimages.Image", on_delete=models.SET_NULL, null=True, blank=True, related_name="+")
+    callToAction = models.ForeignKey("wagtailcore.Page", on_delete=models.SET_NULL, null=True, blank=True, related_name="+")
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('description'),
+        FieldPanel('image'),
+        FieldPanel('callToAction'),
+    ]
+    
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
