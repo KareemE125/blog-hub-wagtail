8e187a8a05b26eaed7794dd1c73d5a73e984c695
KareemE125 <kareemezzat.125@gmail.com>
2023-12-30 11:09:46 +0200

    Init

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mqvgagas+^ypotli=5n6d2s64sx0=%028u(thn)@gatbj-91wb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
