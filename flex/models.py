9a8284f63f57e32175cb4980577f391118e27898
KareemE125 <kareemezzat.125@gmail.com>
2024-01-02 01:30:00 +0200

    Stream Field

--- flex/models.py
+++ flex/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+
+from streams import blocks
 # Create your models here.
 
 class FlexPage(Page):
 
+    content = StreamField([ 
+            ("title_and_text", blocks.TitleAndTextBlock()), 
+        ],use_json_field=True, blank=True, null=True)
     
-    subTitle = models.CharField(max_length=100, blank=True, null=True)
+    subTitle = models.CharField(max_length=100, blank=True, null=True) 
     
     content_panels = Page.content_panels + [
         FieldPanel('subTitle'),
+        FieldPanel('content'),
     ]
