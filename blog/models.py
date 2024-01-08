0a2b6b06fa26f4b0ac8d55f5486514bcfc3de850
KareemE125 <kareemezzat.125@gmail.com>
2024-01-09 01:34:38 +0200

    Add Routable Page (latest blogs)

--- blog/models.py
+++ blog/models.py
@@ -3,23 +3,39 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
+from wagtail.contrib.routable_page.models import RoutablePageMixin, path, re_path
 
 from streams import blocks
 
 # Create your models here.
 
-class BlogsPage(Page):
+class BlogsPage(RoutablePageMixin, Page):
     custom_title = models.CharField(max_length=100, blank=False, null=False, help_text='Custom title for the blog detail page')
 
     content_panels = Page.content_panels + [
         FieldPanel('custom_title'),
     ]
     
+    
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        context["blogs"] = BlogDetailPage.objects.live().public()
+        context["blogs"] = BlogDetailPage.objects.live().public().order_by('-last_published_at')
         
         return context
+    
+    
+    @path('latest/<int:num>/', name='latest_blogs')
+    @path('latest/', name='latest_blogs')
+    def latest_blogs(self, request, num=1):
+        self.custom_title = "Latest Blogs"
+        
+        return self.render(
+            request,
+            template="blog/latest_blogs.html",
+            context_overrides={
+                "blogs": self.get_context(request)["blogs"][:num],
+            },
+        )
 
 
 class BlogDetailPage(Page):
