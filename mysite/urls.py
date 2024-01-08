47918a4a902c15109eaad9a5e226a23efa0627bc
KareemE125 <kareemezzat.125@gmail.com>
2024-01-09 01:56:33 +0200

    add site maps (for search engine)

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 from search import views as search_views
 
@@ -13,6 +14,8 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    path('sitemap.xml', sitemap),
+    re_path(r'', include(wagtail_urls)),
 ]
 
 
