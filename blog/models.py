73e6723194377d830d0f02efaf0498c7a0e0a953
Kareem Ezzat <Kareemezzat.125@gmail.com>
2024-01-09 13:19:04 +0200

    add categories to blog page details
    Add Categories with different way from the way Authors is added by:
    1) Authors added by Orderables
    2) Categories added by ParentalManyToManyField

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django import forms
 
 from wagtail.models import Page, Orderable
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.fields import StreamField
 from wagtail.snippets.models import register_snippet
 from wagtail.contrib.routable_page.models import RoutablePageMixin, path
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 
 from streams import blocks
 
 # Snippet Models
+@register_snippet
 class Author(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     image = models.ForeignKey("wagtailimages.Image", null=True, blank=True, on_delete=models.SET_NULL, related_name="+")
@@ -28,9 +30,28 @@ class Author(models.Model):
         verbose_name = "Author"
         verbose_name_plural = "Authors"
 
-register_snippet(Author)
+
+@register_snippet
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    slug = models.SlugField(max_length=100, blank=False, null=False)
+    
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("slug"),
+    ]
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Blog Category"
+        verbose_name_plural = "Blog Categories"
+        ordering = ["name"]
 
 
+
+# Orderable Models
 class AuthorOrderable(Orderable):
     page = ParentalKey("blog.BlogDetailPage", related_name="blog_authors")
     author = models.ForeignKey("blog.Author", on_delete=models.CASCADE, related_name="+")
@@ -73,6 +94,7 @@ class BlogsPage(RoutablePageMixin, Page):
 class BlogDetailPage(Page):
     
     custom_title = models.CharField(max_length=100, blank=False, null=False, help_text='Custom title for the blog detail page')
+    categories = ParentalManyToManyField("blog.Category", blank=True)
     
     image = models.ForeignKey(
         "wagtailimages.Image",
@@ -95,6 +117,9 @@ class BlogDetailPage(Page):
         MultiFieldPanel([
             InlinePanel('blog_authors', label="Author", min_num=1),
         ], heading="Author(s)"),
+        MultiFieldPanel([
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Category(s)"),
     ]
     
     
